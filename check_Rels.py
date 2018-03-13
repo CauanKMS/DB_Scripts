9e94b91020c90cbaf1c9d1c88bcea5dc546499ad
Cauan.KMS <cauan.kms@gmail.com>
2018-03-13 10:06:12 -0300

    Update check_Rels.py
    A quick update for a better organization of the code.

--- check_Rels.py
+++ check_Rels.py
@@ -1,12 +1,7 @@
 #CauanKMS, Feb 23rd 2018
 
-import os.path, datetime, win32api as win;
-
-###########
-# REPORTS #
-###########
-
-ti = ['0005', '5555', '0555', '0055']
+import os.path, datetime
+import win32api as win
 
 repsSrc = "//sm1/dpout/"
 
@@ -18,16 +13,12 @@ def checkFile(file):
     else:
         return file.lstrip(repsSrc) + ': DAMN, SON! ' + modTimeFile
 
-fileReps = ["r04_", "o2_","019_G_", "pr7_"] 
+ti = ['0005', '5555', '0555', '0055']
+fileReps = ["r04_", "o2_","019_G_", "pr7_"]
 
-for f in fileReps:
-    for t in ti:
-        filepath = repsSrc + f + t + ".txt"
-        print(checkFile(filepath))
-
-###################
+##########
 # BD.mdb #
-###################
+##########
 
 filepath_ap70 = "//ap70/"
 filepathBD = filepath_ap70 + "BD.mdb"
@@ -38,8 +29,20 @@ while(tryagain == True):
     try:
         modTimeFileBD = datetime.datetime.fromtimestamp(os.path.getmtime(filepathBD)).strftime("%d/%m/%Y %H:%M")
         print('\n' + filepathBD.lstrip(filepath_ap70) + ': ONLINE, ' + modTimeFileBD)
-        win.MessageBox(0, 'DONE!', 'BD.mdb is online!')
         tryagain = False
 
     except:
-        print('\n' + filepathBDNotas.lstrip(filepath_ap70) + ': OFFLINE')
+        print('\n' + filepathBD.lstrip(filepath_ap70) + ': OFFLINE')
+        tryagain = True
+
+###########
+# REPORTS #
+###########
+
+for f in fileReps:
+    for t in tipos:
+        filepath = repsSrc + f + t + ".txt"
+        print(checkFile(filepath))
+
+win.MessageBox(0, 'CHECK THE FILES!', 'DONE!')
+
